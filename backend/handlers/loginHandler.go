c335fd6a74a1cc7145533176df4acdeea23099ea
Adewah245 <adewahjames@gmail.com>
2026-07-27 16:54:29 +0100

    login page update

--- backend/handlers/loginHandler.go
+++ backend/handlers/loginHandler.go
@@ -5,16 +5,33 @@ import (
 	"log"
 	"net/http"
 )
-	type LoginData struct {
-		Phone string
-		Password string
-	}
+
+type LoginData struct {
+	Phone    string
+	Password string
+}
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("user Visited Login Page")
-	if err := render.RenderTemplates(w, "login.html", nil); err != nil {
-		log.Println("err render can not load login html")
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
+	switch r.Method {
+	case http.MethodGet:
+		log.Println("User Visited Login Page")
+		if err := render.RenderTemplates(w, "login.html", nil); err != nil {
+			log.Println("Render Error")
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+	case http.MethodPost:
+		user := LoginData{
+			Phone:    r.FormValue("phone"),
+			Password: r.FormValue("password"),
+		}
+		if user.Phone == "" || user.Password == "" {
+			log.Println("Both Phone Number And Password Are Required")
+			http.Error(w, "Bad Request", http.StatusBadRequest)
+		}
+		http.Redirect(w, r, "/dashboard", http.StatusSeeOther)
+	default:
+		http.Error(w, "Method Not Allowed!", http.StatusMethodNotAllowed)
 	}
+
 }
--- backend/routes/registerRoutes.go
+++ backend/routes/registerRoutes.go
@@ -12,9 +12,11 @@ func RegisterRoutes() {
 			http.FileServer(http.Dir("static")),
 		),
 	)
-	http.HandleFunc("/", middleware.Onlypath("/", middleware.OnlyGet(handlers.IndexHandler)))
-	http.HandleFunc("/register", middleware.Onlypath("/", middleware.OnlyGet(handlers.RegisterHandler)))
-	http.HandleFunc("/login", middleware.Onlypath("/", middleware.OnlyGet(handlers.LoginHandler)))
-	http.HandleFunc("/dashboard", middleware.Onlypath("/", middleware.OnlyGet(handlers.DashBoard)))
-	http.HandleFunc("/profile", middleware.Onlypath("/", middleware.OnlyGet(handlers.MarketHandler)))
+	http.HandleFunc("/", middleware.OnlyPath("/", middleware.OnlyGet(handlers.IndexHandler)))
+	http.HandleFunc("/register", middleware.OnlyPath("/", middleware.OnlyGet(handlers.RegisterHandler)))
+	http.HandleFunc("/login", middleware.OnlyPath("/", middleware.OnlyGet(handlers.LoginHandler)))
+	http.HandleFunc("/dashboard", middleware.OnlyPath("/", middleware.OnlyGet(handlers.DashBoard)))
+	http.HandleFunc("/profile", middleware.OnlyPath("/", middleware.OnlyGet(handlers.MarketHandler)))
+	http.HandleFunc("/ai-assistant", middleware.OnlyPath("/", middleware.OnlyGet(handlers.Ai_Assistant)))
+	http.HandleFunc("/profile", middleware.OnlyPath("/", middleware.OnlyGet(handlers.ProfileHandler)))
 }
