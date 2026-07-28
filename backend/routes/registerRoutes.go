40e6e30e2343544bbc8406c374031bb5b8dfc89e
* <root@localhost.localdomain>
2026-07-28 04:51:37 +0100

    update Routes

--- backend/routes/registerRoutes.go
+++ backend/routes/registerRoutes.go
@@ -13,10 +13,10 @@ func RegisterRoutes() {
 		),
 	)
 	http.HandleFunc("/", middleware.OnlyPath("/", middleware.OnlyGet(handlers.IndexHandler)))
-	http.HandleFunc("/register", middleware.OnlyPath("/", middleware.OnlyGet(handlers.RegisterHandler)))
-	http.HandleFunc("/login", middleware.OnlyPath("/", middleware.OnlyGet(handlers.LoginHandler)))
-	http.HandleFunc("/dashboard", middleware.OnlyPath("/", middleware.OnlyGet(handlers.DashBoard)))
-	http.HandleFunc("/profile", middleware.OnlyPath("/", middleware.OnlyGet(handlers.MarketHandler)))
-	http.HandleFunc("/ai-assistant", middleware.OnlyPath("/", middleware.OnlyGet(handlers.Ai_Assistant)))
-	http.HandleFunc("/profile", middleware.OnlyPath("/", middleware.OnlyGet(handlers.ProfileHandler)))
+	http.HandleFunc("/register", middleware.OnlyPath("/register", middleware.OnlyGet(handlers.RegisterHandler)))
+	http.HandleFunc("/login", middleware.OnlyPath("/login", middleware.OnlyGet(handlers.LoginHandler)))
+	http.HandleFunc("/dashboard", middleware.OnlyPath("/dashboard", middleware.OnlyGet(handlers.DashBoard)))
+	http.HandleFunc("/profile", middleware.OnlyPath("/profile", middleware.OnlyGet(handlers.MarketHandler)))
+	http.HandleFunc("/ai-assistant", middleware.OnlyPath("/ai-asistant", middleware.OnlyGet(handlers.Ai_Assistant)))
+	http.HandleFunc("/profile", middleware.OnlyPath("/profile", middleware.OnlyGet(handlers.ProfileHandler)))
 }
