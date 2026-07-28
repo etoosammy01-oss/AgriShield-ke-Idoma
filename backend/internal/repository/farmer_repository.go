e631dc46f84c5692438df985ca868ad75c267ac9
Samuel <akilozisamuel333@gmail.com>
2026-07-28 13:41:34 +0100

    Resolve login validation

---
+++ backend/internal/repository/farmer_repository.go
@@ -0,0 +1,70 @@
+package repository
+
+import (
+	"backend/internal/models"
+	"database/sql"
+)
+
+type FarmerRepository struct {
+	db *sql.DB
+}
+
+func NewFarmerRepository(db *sql.DB) *FarmerRepository {
+	return &FarmerRepository{
+		db: db,
+	}
+}
+
+func (r *FarmerRepository) Create(farmer *models.Farmer) error {
+	query := `
+	INSERT INTO farmers
+	(full_name, phone, password_hash, location)
+	VALUES (?, ?, ?, ?)
+	`
+
+	_, err := r.db.Exec(
+		query,
+		farmer.FullName,
+		farmer.Phone,
+		farmer.PasswordHash,
+		farmer.Location,
+	)
+
+	return err
+}
+
+func (r *FarmerRepository) GetByPhone(phone string) (*models.Farmer, error) {
+
+	query := `
+	SELECT
+		id,
+		full_name,
+		phone,
+		password_hash,
+		location,
+		created_at,
+		updated_at
+	FROM farmers
+	WHERE phone = ?
+	`
+
+	var farmer models.Farmer
+
+	err := r.db.QueryRow(query, phone).Scan(
+		&farmer.ID,
+		&farmer.FullName,
+		&farmer.Phone,
+		&farmer.PasswordHash,
+		&farmer.Location,
+		&farmer.CreatedAt,
+		&farmer.UpdatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &farmer, nil
+}
+
+//func (r *FarmerRepository) GetByID(id int) (*models.Farmer, error)
--- backend/main.go
+++ backend/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"backend/internal/database"
+	"backend/internal/models"
+	"backend/internal/repository"
 	"backend/routes"
 	"log"
 	"net/http"
@@ -23,5 +25,21 @@ func main() {
 	routes.RegisterRoutes()
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Println(err)
+	}
+
+	farmerRepo := repository.NewFarmerRepository(db)
+
+	farmer := &models.Farmer{
+		FullName:     "Test Farmer",
+		Phone:        "08011112222",
+		PasswordHash: "hashed-password",
+		Location:     "Otukpo",
+	}
+
+	err = farmerRepo.Create(farmer)
+	if err != nil {
+		log.Println("Insert failed:", err)
+	} else {
+		log.Println("Farmer inserted successfully")
 	}
 }
