1389d6462465010a401a97aa71c3a03dce70d314
Adewah245 <adewahjames@gmail.com>
2026-07-28 17:05:19 +0100

    my asset page

--- backend/go.mod
+++ backend/go.mod
@@ -4,4 +4,6 @@ go 1.25.0
 
 require github.com/mattn/go-sqlite3 v1.14.48
 
-require golang.org/x/crypto v0.54.0 
+require golang.org/x/crypto v0.54.0
+
+require github.com/google/uuid v1.6.0 // indirect
--- backend/handlers/market.go
+++ backend/handlers/market.go
@@ -2,17 +2,33 @@ package handlers
 
 import (
 	"backend/render"
+	//"backend/internal2"
 	"log"
 	"net/http"
 )
 type MainMarket struct {
-	
+	ID string
+	Name string
+	Location string
+	State string
 }
 func MarketHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("User visited Market Page")
-	if err := render.RenderTemplates(w, "market.html", nil); err != nil {
-		log.Fatalln("Render Not Loaded", err)
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
+	switch r.Method {
+	case http.MethodGet:
+		log.Println("User Visited Market Page")
+		if err := render.RenderTemplates(w, "market.html", nil); err != nil {
+			log.Println("Render Error", err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+	case http.MethodPost:
+     User := MainMarket {
+		ID: r.FormValue("id"),
+		Name: r.FormValue("name"),
+		Location: r.FormValue("location"),
+		State: r.FormValue("state"),
+	 }
+	 
+	 http.Redirect(w,r, User.ID, http.StatusSeeOther)
 	}
 }
